c176bd777a5085f508e8d40ba0e122e0296bbb29
Desiah Barnett <desiahbarnett@gmail.com>
2024-01-31 19:31:56 -0500

    completedlab

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./components/App"; // Will attach App.js to index.js
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
